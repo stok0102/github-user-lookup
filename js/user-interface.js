790fd124d7ece8e1d881c1b7cafa4944711e17f9
Caleb Stokka <caleb.stokka@gmail.com>
2016-08-05 09:45:55 -0700

    add function to display username and avatar on page

--- js/user-interface.js
+++ js/user-interface.js
@@ -3,8 +3,7 @@ var User = require('./../js/user.js').userModule;
 $(document).ready(function () {
   $('#getUser').click(function () {
     var usernameInput = $('#nameSearch').val();
-    newUser = new UserSearch();
-    debugger;
+    newUser = new User();
     newUser.getUser(usernameInput);
   });
 });
--- js/user.js
+++ js/user.js
@@ -5,6 +5,8 @@ function User() {}
 User.prototype.getUser = function (usernameInput) {
   $.get('https://api.github.com/users/' + usernameInput + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    $('.userInfo h1').text(response.name);
+    $('.userInfo').append("<img class='img-responsive center-block' src='" + response.avatar_url + "'/>");
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
